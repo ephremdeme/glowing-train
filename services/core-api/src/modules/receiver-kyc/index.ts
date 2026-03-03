238632fc61dd9ea4ee0ed88af0443c991a9cdd69
Ephrem Demelash <demelashephrem@gmail.com>
2026-03-03 09:41:45 +0300

    refactor(core-api): remove receiver kyc module

--- services/core-api/src/modules/receiver-kyc/index.ts
+++
@@ -1,3 +0,0 @@
-export { ReceiverKycRepository } from './repository.js';
-export { ReceiverKycService } from './service.js';
-export type { ReceiverKycProfile, UpsertReceiverKycInput } from './types.js';
